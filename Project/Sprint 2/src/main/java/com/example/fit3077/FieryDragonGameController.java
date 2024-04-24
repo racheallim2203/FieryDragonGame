f5f484d046a2b8becc1ae5b625cb3f202e7e05ee
Racheal <racheal2203@gmail.com>
2024-04-24 21:59:41 +1000

    Set up main() with css stylesheet initiate and inisitating stage and scnene

---
+++ Project/Sprint 2/src/main/java/com/example/fit3077/FieryDragonGameController.java
@@ -0,0 +1,16 @@
+package com.example.fit3077;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FieryDragonGameController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- Project/Sprint 2/src/main/java/com/example/fit3077/Main.java
+++ Project/Sprint 2/src/main/java/com/example/fit3077/Main.java
@@ -2,21 +2,37 @@ package com.example.fit3077;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Group;
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("fiery-dragon-game.fxml"));
+        Scene scene = new Scene(root);
+        String css = this.getClass().getResource("style.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setTitle("Fiery Dragon Game");
+
+        try {
+            Image icon = new Image(getClass().getResourceAsStream("/BabyDragon.png"));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.err.println("Icon not found: " + e.getMessage());
+        }
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+
+
 }
