896c87aee372bc397a591fab7ea4df86ed51af90
Racheal <racheal2203@gmail.com>
2024-04-25 08:36:10 +1000

    Updated covered cards view on game board

--- Project/Sprint 2/src/main/java/com/example/fit3077/FieryDragonGameController.java
+++ Project/Sprint 2/src/main/java/com/example/fit3077/FieryDragonGameController.java
@@ -12,76 +12,37 @@ import java.util.ResourceBundle;
 
 public class FieryDragonGameController implements Initializable {
 
-    @FXML
-    private FlowPane boardcards;
+        @FXML
+        private FlowPane boardcards;
 
-    @FXML
-    private FlowPane cardsDeck;
+        @FXML
+        private FlowPane cardsDeck;
 
-    @FXML
-    private Label currentPlayer;
+        @FXML
+        private Label currentPlayer;
 
-    @FXML
-    private ImageView dragon;
+        @FXML
+        private ImageView dragon;
 
-    @FXML
-    private ImageView dragon1;
+        @FXML
+        private ImageView fish;
 
-    @FXML
-    private ImageView dragon2;
+        @FXML
+        private FlowPane gameBoard;
 
-    @FXML
-    private ImageView dragon3;
+        @FXML
+        private ImageView octopus;
 
-    @FXML
-    private ImageView fish;
+        @FXML
+        private ImageView pufferfish;
 
-    @FXML
-    private ImageView fish1;
+        @FXML
+        private ImageView uncoveredCards;
 
-    @FXML
-    private ImageView fish2;
+        @FXML
+        void startGame(ActionEvent event) {
 
-    @FXML
-    private ImageView fish3;
-
-    @FXML
-    private FlowPane gameBoard;
-
-    @FXML
-    private ImageView octopus;
-
-    @FXML
-    private ImageView octopus1;
-
-    @FXML
-    private ImageView octopus2;
-
-    @FXML
-    private ImageView octopus3;
-
-    @FXML
-    private ImageView pirate1;
-
-    @FXML
-    private ImageView pirate2;
-
-    @FXML
-    private ImageView puffer1;
-
-    @FXML
-    private ImageView puffer2;
-
-    @FXML
-    private ImageView puffer3;
-
-    @FXML
-    private ImageView pufferfish;
-
-    @FXML
-    void startGame(ActionEvent event) {
-
-    }
+        }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
