1642c4170f7037679c9a504dfaf1db6bc25f15de
Racheal <racheal2203@gmail.com>
2024-04-28 16:40:21 +1000

    Updated README File instructions to run software prototype

--- Project/Sprint 2/src/main/java/com/example/fit3077/AnimalCard.java
+++ Project/Sprint 2/src/main/java/com/example/fit3077/AnimalCard.java
@@ -10,21 +10,6 @@ public class AnimalCard extends Card {
     }
 
 
-//    @Override
-//    public boolean applyEffect(Player player, GameMap gameMap) {
-//        // retrieves the player’s current Habitat from the GameMap using the player's position.
-//        Habitat currentHabitat = gameMap.getHabitat(player.getPosition());
-//
-//        // Check if the current habitat contains an animal card that matches the type of this animal card
-//        if (currentHabitat.containsAnimal(getType())) {
-//            player.moveToken(this.count, gameMap); // If match, move the player token forward
-//            return true; // Move was successful, turn continues
-//        } else {
-//            // No match, player must wait until the next turn, card effect has been applied but turn ends
-//            return false; // Move was not successful, turn ends
-//        }
-//    }
-
     @Override
     public void applyEffect(Player currentPlayer, GameMap gameMap, Card card) {
         int forwardSteps = card.getCount();
